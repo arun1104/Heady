967355e55d3caa0d2defb977d0b7eddda3aab266
unknown <arunrajeevan1104@gmail.com>
2020-07-04 17:39:26 +0530

    added tests

--- test/unit/controllers/UT_insertMany_test.js
+++ test/unit/controllers/UT_insertMany_test.js
@@ -13,21 +13,19 @@ if (mocha.describe) {
 }
 let dbLayer = require('../../../utilities/mongodbLayer');
 
-describe('create category positive suit', async function() {
+describe('insert many positive suit', async function() {
   it('should return valid values', async function() {
     dbLayer.Mongoose = {};
     dbLayer.Mongoose.connect = sinon.stub();
     dbLayer.Mongoose.model = sinon.stub();
     dbLayer.Mongoose.connect.resolves();
-    dbLayer.Mongoose.model.returns(function() {
-      this.save = sinon.stub();
-      this.save.resolves();
-      this.categoryId = '123';
-    });
+    let insertManyMock = sinon.stub();
+    insertManyMock.resolves({prodId: 1});
+    dbLayer.Mongoose.model.returns({insertMany: insertManyMock});
     try {
       let reqObj = { userId: '123' };
-      let result = await dbLayer.createDoc({ data: reqObj }, '7i68gduygwduvwd', 'category');
-      assert.equal(result.categoryId, '123');
+      let result = await dbLayer.insertMany({data: [{ data: reqObj }]}, '7i68gduygwduvwd', 'category');
+      assert.equal(result.prodId, 1);
     } catch (err) {
       assert.fail('Exception in getReviewsDBLayer Positive suit');
     }
